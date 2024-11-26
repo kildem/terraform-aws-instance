c8998a69eea2ae59e00bfd0b63de29293d916458
kildem <kilden777@gmail.com>
2024-11-26 11:10:02 +0100

    add outputs and variables tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 resource "aws_instance" "hello_word" {
   ami = data.aws_ami.ubuntu.id
   subnet_id = data.aws_subnets.default.ids[0]
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_nat_gateway" "example" {
+  subnet_id = data.aws_subnets.default.id
+
+  depends_on = [aws_internet_gateway.main]
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {
+  type = string
+  description = "The type of instance to launch."
+  default = "t3.micro"
+}
